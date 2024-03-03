09d2f6d544d98e147c8e370432e788149a579f12
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-03 02:12:02 +0000

    Adding two money amountn in cents

---
+++ Week02/bank.py
@@ -0,0 +1,16 @@
+# bank.py
+# Program reads two money amounts an print the answer with a Euro sign
+# Author: Noemi Diaz
+
+r = float (0.65)
+z = float (1.80) 
+print (r + z)
+
+# Print out the answer in a human readable with a euro sign
+price = 2.45
+txt = "The price is {} Euros"
+print (txt.format (price))
+
+
+
+
