4db423f7b2cf2833adbf3b7a3900318aa8e218bd
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-12 20:37:52 +0000

    trying to upload masked number

--- Week03/accounts.py
+++ Week03/accounts.py
@@ -4,4 +4,15 @@
 
 # Step 1: Input account number
 
-account_number= input ("1020304050")
+account_number = input ("1020304050")
+
+# Step 2: Checking the account number lenght
+
+if len(account_number) !=10:
+    print("1020304050")
+
+
+# Step 3: Masking account number replacing first characters with 'X'
+    
+    masked_number = "X" * 6 + account_number[-4:]
+    print("Masked Account Number:", masked_number)
