2f9772c6606631ee516ee509be9fbdec9ee9fea5
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-12 20:06:40 +0000

    input account number variable

--- Week03/accounts.py
+++ Week03/accounts.py
@@ -1,3 +1,13 @@
 # accounts.py
 # Program that takes a 10-character account number and displays it with only the last 4 digits visible, while first 6 digits are replaced with Xs
 # Author: Noemi Diaz
+
+# Step 1: Input account number
+
+account_number= input ("1020304050")
+
+# Step 2: Check aacount number length
+
+if len(account_number) !=10: 
+
+# Step 3: Masking account number
