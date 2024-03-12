a7f421eea221ed6db21260c501b1b4b8acd237df
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-12 19:12:50 +0000

    Description of the task

--- Week03/accounts.py
+++ Week03/accounts.py
@@ -1,3 +1,3 @@
 # accounts.py
-#
+# Program that takes a 10-character account number and displays it with only the last 4 digits visible, while first 6 digits are replaced with Xs
 # Author: Noemi Diaz
