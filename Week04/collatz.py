986bf29dcb91f1f48fef1b9ba652705fbe6e1ef7
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-24 17:53:27 +0000

    ask user to enter a number integer

--- Week04/collatz.py
+++ Week04/collatz.py
@@ -1,3 +1,27 @@
 # collatz.py
 #
 #Author:Noemi Diaz
+
+
+
+# Definitizion of variable and sequence depending if number is even or odd
+
+def collatz (number):
+    while number > 1:
+        print (number, end='')
+       # n is even
+        if number % 2 == 0:
+            number = number // 2
+        # n is odd
+        else:
+            number = number * 3 + 1
+    print(1)
+
+# Ask user to enter a number- Integer
+
+n = int (input ('Enter number:'))
+
+print (number,end= '')
+
+collatz(number)
+
