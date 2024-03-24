91b790a90e115a4fc243f9fe6a04ae408e30c50e
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-24 20:07:31 +0000

    Describing steps of program

--- Week05/weekday.py
+++ Week05/weekday.py
@@ -2,15 +2,23 @@
 #
 # Author: Noemi Diaz
 
-#Import python's datetime
+
+# Step 1: Import python's datetime
 
 import datetime
 
-# weekdays definition
+
+# Step 2: Checking days of the week as a tuple or integer from 0 (Monday) to 6 (Sunday)
+
 weekday = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
 
-# Date of today
+
+# Step 3: Date of today
+
 today = datetime.datetime.today()
+
+
+# Step 4: Describing conditional statements
 
 if today.weekday() == 4:
 
@@ -19,4 +27,3 @@ if today.weekday() == 4:
 elif today.weekday() == 5 or today.weekday() == 6:
     
     print ("Hooray!It's finally weekend!")
-    
