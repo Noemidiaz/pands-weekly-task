c50ed0ce9093e2456cbf7ed56981ff2b9ce349ac
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-24 19:37:41 +0000

    import python's datetime

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pands-weekly-task
+
+
+# week05 
+https://www.sanfoundry.com/python-program-test-collatz-conjecture-given-number/
--- Week05/weekday.py
+++ Week05/weekday.py
@@ -1,3 +1,12 @@
 # weekday.py
 #
 # Author: Noemi Diaz
+
+#Import python's datetime
+
+import datetime
+
+# weekdays definition
+weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+day_of_week = today.weekday ()
