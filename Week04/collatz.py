4ae470443255207f7501816641e2d89383bc97ea
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-24 18:24:54 +0000

    describing program

--- Week04/collatz.py
+++ Week04/collatz.py
@@ -1,5 +1,5 @@
 # collatz.py
-#
+# In this program the user enter any positive integer and the program ends when the value is 1
 #Author:Noemi Diaz
 
 
@@ -7,12 +7,15 @@
 # Definitizion of function 'collatz' with a 'number' value
 
 def collatz (number):
+
    # Adding a loop using 'while' until the number becomes 1
     while number > 1:
         print (number, end=' ')
+      
        # n is odd
         if (number % 2):
             number = number * 3 + 1  
+       
         # n is even
         else:
             number = number//2
