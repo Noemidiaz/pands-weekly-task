5a8381979efafb75074bba7d8f0ada1a4097f766
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-13 16:35:44 +0100

    request users number

--- Week06/squareroot.py
+++ Week06/squareroot.py
@@ -1,3 +1,22 @@
 # squareroot.py
 # Create a program that takes a floating positive number and gives an approximate square root using Newton's method
 # Author: Noemi Diaz
+
+
+#Defining function as sqrt
+def sqrt (number):
+   
+    #Initial  estimation for the square root
+   estimateData = number / 2
+
+    #Continue iterating until the estimation  is sufficiently accurate
+   while abs (estimateData * estimateData - number) > 0.0001:
+     
+     
+     #Improving the estimation using Newton's method
+     estimateData = (estimateData + number / estimateData) /2 
+     return estimateData
+
+#Request user number
+   number = float (input ("Enter a positive floating-point numner: "))
+   
