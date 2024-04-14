b5dfc384e1f2f4a867340ec63431078d51092aab
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-14 20:44:11 +0100

    adding histogram color

--- Week08/plottask.py
+++ Week08/plottask.py
@@ -2,7 +2,7 @@
 # Program that uses matplotlib to make a histogram of a normal distribution and a plot of the function h(x)=x3.
 # Author: Noemi Diaz
 
-pip install matplotlib
+
 
 # Task1. Program that displays a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2.
 
@@ -16,12 +16,18 @@ np.random.seed(1)
 data = np.random.normal(5, 2, 1000)
 
 # Plot histogram
-plt.hist(data)
+plt.hist(data, color= 'lightgreen')
+
+#Adding detail to histogram (Legend, color)
+plt.title ('Healthy life style Histogram')
+plt.xlabel("sports activities")
+plt.ylabel("frequency")
+plt.legend()
+
+# Show the plot
 plt.show()
 
 
 
 # Task2. Program that displays a plot of the function  h(x)=x3 in the range 0 to 10.
 
-import numpy as np
-import matplotlib.pyplot as plt
