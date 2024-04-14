19f0145d20f87c54baac3ad124aef4d5f169bf76
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-14 21:27:06 +0100

    correcting grammar errors

--- Week08/plottask.py
+++ Week08/plottask.py
@@ -32,7 +32,7 @@ plt.show()
 
 '''
 # TASK2.
-# a plot of the function  h(x)=x3 in the range 0 to 10
+# Program that displays a plot of the function  h(x)=x3 in the range 0 to 10
 
 import numpy as np
 import matplotlib.pyplot as plt
