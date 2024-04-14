81d0a8f296ced68c24b2d7794e289674d4938160
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-14 16:57:40 +0100

    correcting errors prompt user

--- Week07/es.py
+++ Week07/es.py
@@ -22,16 +22,19 @@ def count_letter_in_file(filename, letter):
         print("Error:File not found.")
         return -1
     
-    # File path / filename of the text file. Prompt the user name of file
-    filename = input("Enter the name of the text file: ")
+# File path / filename of the text file. Prompt the user name of file
+filename = input("Enter the name of the text file: ")
 
-    # Asking for the letter to count
-    count_letter_in_file = input("which letter would you like to count?: ")
+# Asking for the letter to count
+count_letter_in_file = input("which letter would you like to count?: ")
 
-    # 
-    if result != -1:
-        print (f "The letter'{letter_to_count}' appears }{}")
-    print(f"The letter '{letter_to_count}' appears {result} times in the file.")
+# Function to result the ocurrences of 'Letter'
+result = count_letter_in_file(filename)
+
+# Display result 
+if result != -1:
+    print (f"The letter'{letter_to_count}' appears {result} times in the file.")
+    
 
 
         
