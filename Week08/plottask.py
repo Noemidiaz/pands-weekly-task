a45a0dc8d13be9c7bb186b779c2b7aea5a83738a
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-14 21:06:15 +0100

    adding  plot details

--- Week08/plottask.py
+++ Week08/plottask.py
@@ -3,7 +3,7 @@
 # Author: Noemi Diaz
 
 
-
+'''
 # Task1. Program that displays a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2.
 
 import numpy as np
@@ -18,7 +18,7 @@ data = np.random.normal(5, 2, 1000)
 # Plot histogram
 plt.hist(data, color= 'lightgreen')
 
-#Adding detail to histogram (Legend, color)
+#Adding details to histogram
 plt.title ('Healthy life style Histogram')
 plt.xlabel("sports activities")
 plt.ylabel("frequency")
@@ -28,6 +28,35 @@ plt.legend()
 plt.show()
 
 
+'''
 
 # Task2. Program that displays a plot of the function  h(x)=x3 in the range 0 to 10.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the range 0 to 10 values
+x = np.linspace(0, 10, 100)
+
+
+# Define the range of x values
+x = np.linspace(0, 10, 100)
+
+# Calculate the values for y corresponding  for h(x) = x^3
+h_x = x ** 3
+
+# Plot the function
+plt.plot(x, h_x, color='orange', linewidth=2)
+
+# Adding details
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.title('The influence of professional experience in salary')
+plt.legend()
+
+# Show the plot
+plt.grid(True)
+plt.show()
+
+
 
