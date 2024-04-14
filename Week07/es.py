f75ccc240364273f9228cefd509e86789a45d988
Noemi Diaz <noemi.diazglez@gmail.com>
2024-04-14 14:34:28 +0100

    new function

--- Week07/es.py
+++ Week07/es.py
@@ -6,13 +6,31 @@
 
 
 def count_letter_in_file(filename, letter):
+    
     try :
-        with open ('mobydick.txt', 'r') as file:
+        # open the file in read mode
+        with open (filename, 'r') as file:
+
             # Read the contents of the file
             text = file.read()
-            # Count occurrences of 'e' (both uppercase and lowercase)
-            count = text.lower().count('e')
+
+            # Count occurrences of 'letter' (both uppercase and lowercase)
+            count = text.count(letter)
             return count
+        
     except FileNotFoundError:
         print("Error:File not found.")
+        return -1
+    
+    # File path / filename of the text file. Prompt the user name of file
+    filename = input("Enter the name of the text file: ")
+
+    # Asking for the letter to count
+    count_letter_in_file = input("which letter would you like to count?: ")
+
+    # 
+    if result != -1:
+
+
+
         
